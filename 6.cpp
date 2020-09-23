86c329082f410e608e2f53bb26f8f5f2ebc9f309
Tareq Hasan <saj99880@gmail.com>
2020-09-23 11:28:03 +0600

    Initial commit

---
+++ 6.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<cstdlib>
+#include<conio.h>
+using namespace std;
+int main()
+{
+char option;
+
+switch(option)
+{
+case '1':
+break;
+case '2':
+break;
+case '3':
+break;
+case '4':
+break;
+case '5':
+break;
+case '6':
+break;
+}
+
+
+
+cout<<"\n ++++++++++++++++++++++++++++++++ EMPLOYEE MANAGEMENT SYSTEM ++++++++++++++++++++++++++++++++++++++";
+cout<<endl;
+
+cout<<"\n\n \t\t\t\t 1:-BUILT THE EMPLOYEE TABLE ";
+
+cout<<"\n\n\t\t\t\t 2:-LIST THE EMPLOYEE TABLE ";
+
+cout<<"\n\n\t\t\t\t 3:-INSERT NEW ENTRY";
+
+
+cout<<"\n\n\t\t\t\t 4:-DELETE AN ENTRY ";
+
+
+cout<<"\n\n\t\t\t\t 5:-EDIT AN ENTRY";
+
+cout<<"\n\n\t\t\t\t 6:-SEARCH A RECORD ";
+
+
+cout<<"\n\n\t\t\t\t 0:-QUIT PROGRAM";
+
+
+cout<<"\n\n \t\t  -------+++ SELECT YOUR OPTION PLEASE +++-------";
+
+getch();
+return 0;
+}
+
+
